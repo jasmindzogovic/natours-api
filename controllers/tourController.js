da783102d056a464db2df95b06c2b132eaa9954c
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-12-08 12:39:34 -0500

    created own middleware for data validation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,11 +6,21 @@ const tours = JSON.parse(
 
 exports.checkID = (req, res, next, val) => {
   console.log(val);
-  
+
   if (+req.params.id > tours.length) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'No name or price',
     });
   }
   next();
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,12 +3,19 @@ const express = require('express');
 const router = express.Router();
 
 const tourController = require('../controllers/tourController');
-const { getAllTours, createTour, getTour, updateTour, deleteTour, checkID } =
-  tourController;
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  checkID,
+  checkBody,
+} = tourController;
 
 router.param('id', checkID);
 
-router.route('/').get(getAllTours).post(createTour);
+router.route('/').get(getAllTours).post(checkBody, createTour);
 
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
