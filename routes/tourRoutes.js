72ec52b753734448ee08360029619b900f94459d
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-12-12 16:25:53 -0500

    created a schema and model for project database

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const tourController = require('../controllers/tourController');
+
 const {
   getAllTours,
   createTour,
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/userController');
+
 const { getAllUsers, createUser, getUser, updateUser, deleteUser } =
   userController;
 
