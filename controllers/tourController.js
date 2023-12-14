5b744be80f6dd2229ae5a181dbe2ef0e568e0ca2
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-12-14 15:15:53 -0500

    implemented pagination for project

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -11,6 +11,7 @@ exports.getAllTours = async (req, res) => {
     // ADVANCED FILTERING
     // eslint-disable-next-line prefer-const
     let queryString = JSON.stringify(queryObject);
+
     queryString = queryString.replace(
       /\b(gte|gt|lte|lt)\b/g,
       (match) => `$${match}`,
@@ -25,6 +26,27 @@ exports.getAllTours = async (req, res) => {
       query = query.sort(sortBy);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    // FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    // PAGINATION
+
+    const page = +req.query.page || 1;
+    const limit = +req.query.limit || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
     }
 
     const tours = await query;
