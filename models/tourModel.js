c2ab75577bed6f9e0e917870e0788b02765dbc96
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-12-16 11:49:36 -0500

    implemented validators for project database

--- models/tourModel.js
+++ models/tourModel.js
@@ -9,6 +9,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must be under 40 characters'],
+      minlength: [10, 'A tour name must have more than 10 characters'],
     },
     slug: String,
     duration: {
@@ -22,10 +24,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'A ratings minimum value must be 1.0'],
+      max: [1, 'A ratings maximum value must be 5.0'],
     },
     ratingsQuantity: {
       type: Number,
