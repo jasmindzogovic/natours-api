383e5000da1d95840e2ec85485d734b1bc924953
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2023-12-27 13:33:56 -0500

    implemented parameter pollution prevention functionality

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const errorController = require('./controllers/errorController');
@@ -31,6 +34,26 @@ app.use('/api', limiter);
 
 // BODY PARSER
 app.use(express.json({ limit: '10kb' }));
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION CORS
+app.use(xss());
+
+// PREVENT PARAMETER POLLUTION
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 // SERVING STATIC FILES
 app.use(express.static(`${__dirname}/public`));
