526df9acd9ffb729f11865496ff4c224d63767d6
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-01-08 10:07:12 -0500

    install pug for templating

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -15,7 +16,14 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) MIDDLEWARES
+
+// SERVING STATIC FILES
+app.use(express.static(path.join(__dirname, 'public')));
+
 // DEVELOPMENT LOGGING
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -56,9 +64,6 @@ app.use(
   }),
 );
 
-// SERVING STATIC FILES
-app.use(express.static(`${__dirname}/public`));
-
 // EXERCISE MIDDLEWARE
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -66,6 +71,10 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
+
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
