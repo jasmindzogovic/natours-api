2674b65e42a8eb7e5537fb4acc21e9c770a107d8
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-01-09 16:48:11 -0500

    finished implementing mapbox into the project

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,4 +1,45 @@
 /* eslint-disable */
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamFzbWluZHpvZ292aWMiLCJhIjoiY2xyNnRzMmhhMmRrMDJqcWJzeTNwNm1xNSJ9.0RqhcwwlRaOYET3oIPP_TQ';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/jasmindzogovic/clr6umruu012101qubdi85gon',
+  zoom: 3,
+  scrollZoom: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 150,
+    left: 100,
+    right: 100,
+  },
+});
