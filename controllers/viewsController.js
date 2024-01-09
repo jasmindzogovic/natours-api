82f44adbe7e2729694449ecbe1633fd1642e4b64
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-01-09 13:47:37 -0500

    built part one of the tour page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,19 +2,23 @@ const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
-  // 1) Get tour data from collection
   const tours = await Tour.find();
-  // 2) Build the template
 
-  // 3) Render that template using tour data
   res.status(200).render('overview', {
     title: 'All Tours',
     tours,
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res) => {
+  const { slug } = req.params;
+
+  const tour = await Tour.findOne({ slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  // const tour = await Tour.find({slug: })
+  res.status(200).render('tour', {
+    tour,
+  });
+});
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,6 +4,6 @@ const { getOverview, getTour } = require('../controllers/viewsController');
 
 router.get('/', getOverview);
 
-router.get('/tour', getTour);
+router.get('/tour/:slug', getTour);
 
 module.exports = router;
