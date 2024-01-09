3e0727649fda0979cbd2296034efeacb8b3df4bf
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-01-09 14:46:20 -0500

    built the second part of the tour page

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(helmet());
 
 // LIMIT REQUESTS FROM SAME API
 const limiter = rateLimit({
-  max: 100,
+  max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour.',
 });
