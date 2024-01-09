e6ee45ecbc4092777fc19c96c2df04158d6b2bc6
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-01-09 16:49:35 -0500

    changed the rate limit for api requests to website

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(helmet());
 
 // LIMIT REQUESTS FROM SAME API
 const limiter = rateLimit({
-  max: 1000,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour.',
 });
