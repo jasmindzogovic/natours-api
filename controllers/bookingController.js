43a58f60f84ab299a08a3638658e6c11f8417681
Jasmin Dzogovic <jasminxhogovic@gmail.com>
2024-01-23 13:18:02 -0500

    fixed minor code bugs

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -33,7 +33,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
           product_data: {
             name: `${tour.name} Tour`,
             description: tour.summary,
-            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+            images: [`${req.protocol}://${req.get('host')}/${tour.imageCover}`],
           },
         },
         quantity: 1,
@@ -50,8 +50,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items.at(0).price_data.unit_amount / 100;
+  const user = (await User.findOne({ email: session.email })).id;
+  const price = session.data.object.amount_total / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -68,7 +68,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${error.message}`);
   }
 
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
